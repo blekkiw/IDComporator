f8f224f9665dfaae56b73af7fdbd2194638acc2c
dmitrii <dmitrii.golikov91@gmail.com>
2020-01-13 20:28:12 +0200

    buffer test

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,12 @@
+import com.sun.corba.se.impl.ior.ByteBuffer;
+
 import javax.swing.*;
 import java.io.*;
 import java.nio.file.Paths;
+import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.Scanner;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -12,9 +16,9 @@ public class Main {
     public static void main(String[] args) throws IOException {
         String path = pathGenerator();
         String[] files = fileScanner(path);
-        ArrayList <Boolean> compares  = new ArrayList<>();
-        for (int i = 1; i < files.length ; i++) {
-            compares.add(comparator(fromFile(path,files[i]), fromZip(path,files[0],files[i])));
+        ArrayList<Boolean> compares = new ArrayList<>();
+        for (int i = 1; i < files.length; i++) {
+            compares.add(comparator(fromFile(path, files[i]), fromZip(path, files[0], files[i])));
         }
 
         if (!compares.contains(false)) {
@@ -26,83 +30,103 @@ public class Main {
     }
 
 
-
-    private static String [] fileScanner (String path) {
+    private static String[] fileScanner(String path) {
         File dir = new File(path);
         File[] files = dir.listFiles();
-
-        if (files.length<2||files==null) {
+        long timestamp = System.currentTimeMillis();
+        if (files.length < 2 || files == null) {
             throw new RuntimeException("Need more than 1");
         }
-        String [] result = new String[files.length];
+        String[] result = new String[files.length];
         for (File file : files) {
-            String [] names = file.getName().split("\\.");
+            String[] names = file.getName().split("\\.");
             if (names[1].equals("asice")) {
-                result[0]=file.getName();
+                result[0] = file.getName();
             } else {
-                for (int i = 1; i < result.length ; i++) {
-                    if (result[i]==null) {
-                        result[i]=file.getName();
+                for (int i = 1; i < result.length; i++) {
+                    if (result[i] == null) {
+                        result[i] = file.getName();
                         break;
                     }
                 }
             }
         }
-        if (result[0]==null) {
+        if (result[0] == null) {
             throw new RuntimeException("Not found asice file!");
         }
+        System.out.println("file scanner:" + (System.currentTimeMillis() - timestamp));
         return result;
 
     }
 
-    private static String pathGenerator () {
+    private static String pathGenerator() {
         if (osIsWin()) {
-            return System.getProperty("user.dir")+ "\\input\\";
-        } else  return   System.getProperty("user.dir")+ "/input/";
+            return System.getProperty("user.dir") + "\\input\\";
+        } else return System.getProperty("user.dir") + "/input/";
     }
 
-    private static boolean osIsWin () {
+    private static boolean osIsWin() {
         String os = System.getProperty("os.name").toLowerCase();
         return os.contains("win");
     }
 
-    private static String fromFile (String path, String fileName) throws IOException {
-        String filePath = path+fileName;
-        FileInputStream fileInputStream = new FileInputStream(filePath);
+    private static String fromFile(String path, String fileName) throws IOException {
+        long timestamp = System.currentTimeMillis();
+        String filePath = path + fileName;
+//        FileInputStream fileInputStream = new FileInputStream(filePath);
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(filePath));
         String result = "";
-        for (int c = fileInputStream.read(); c!=-1; c=fileInputStream.read()) {
-            result+=c;
+//        for (int c = fileInputStream.read(); c != -1; c = fileInputStream.read()) {
+//            result += c;
+//        }
+        String s="";
+        s= bufferedReader.readLine();
+        while (s!=null) {
+            result+=s.getBytes();
+            s=bufferedReader.readLine();
         }
-return result;
+bufferedReader.close();
+        System.out.println("from file:" + (System.currentTimeMillis() - timestamp));
+        System.out.println(result);
+        return result;
     }
 
-    private static boolean comparator (String fromZip, String fromDir) {
+    private static boolean comparator(String fromZip, String fromDir) {
         return fromDir.equals(fromZip);
+
     }
 
 
-    private static String fromZip (String path, String zipName, String fileName) throws IOException {
-        String filePath = path+zipName;
-        ZipFile zipFile = new ZipFile(filePath);
-        zipFile.getEntry(fileName);
+    private static String fromZip(String path, String zipName, String fileName) throws IOException {
+        long timestamp = System.currentTimeMillis();
+        FileInputStream fileInputStream = new FileInputStream(path + zipName);
+        ZipInputStream zipInputStream = new ZipInputStream(new BufferedInputStream(fileInputStream));
 
-        FileInputStream fileInputStream = new FileInputStream(filePath);
-        ZipInputStream zipInputStream = new ZipInputStream(fileInputStream);
         ZipEntry zipEntry = zipInputStream.getNextEntry();
         String result = "";
-        while (zipEntry!=null) {
+        while (zipEntry != null) {
             if (!zipEntry.isDirectory()) {
                 if (zipEntry.getName().equals(fileName)) {
-                   for (int c = zipInputStream.read(); c!=-1; c= zipInputStream.read()) {
-                       result+=c;
-                   }
+//                    for (int c = zipInputStream.read(); c != -1; c = zipInputStream.read()) {
+//                        result += c;
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    byte[] buffer = new byte[1024];
+                    int count;
+                    // reading and writing
+                    while ((count = zipInputStream.read(buffer)) != -1) {
+                        baos.write(buffer, 0, count);
+                        result+=baos.toByteArray();
+                        baos.reset();
+                    }
                 }
             }
-            zipEntry=zipInputStream.getNextEntry();
+            zipEntry = zipInputStream.getNextEntry();
         }
+zipInputStream.closeEntry();
+        System.out.println(result);
         fileInputStream.close();
         zipInputStream.close();
-
-return result;
+        System.out.println("from zip:" + (System.currentTimeMillis() - timestamp));
+        return result;
     }
 }
